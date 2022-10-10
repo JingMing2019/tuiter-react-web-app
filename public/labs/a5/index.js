9c1e21b80c8707d7dafc1032a9158dbaf829370d
jingming <mary.j.ming@gmail.com>
2022-10-09 22:00:35 -0700

    complete a5 javascript section 2.4.1 variable types

--- public/labs/a5/index.js
+++ public/labs/a5/index.js
@@ -1,2 +1,29 @@
 // alert('Hello World!');
 console.log('Hello World!');
+
+console.log('Variables and Constants');
+let global1 = 10;
+var functionScoped = 2;
+let blockScoped = 5;
+const constant1 = global1 + functionScoped - blockScoped;
+// Print the variables and constants
+console.log(global1);
+console.log(functionScoped);
+console.log(blockScoped);
+console.log(constant1);
+
+console.log('Variable types');
+let numberVariable = 123;
+let floatingPointNumber = 234.345;
+let stringVariable = 'Hello World!';
+let booleanVariable = true;
+let isNumber = typeof numberVariable;
+let isString = typeof stringVariable;
+let isBoolean = typeof booleanVariable;
+console.log(numberVariable);
+console.log(floatingPointNumber);
+console.log(stringVariable);
+console.log(booleanVariable);
+console.log(isNumber);
+console.log(isString);
+console.log(isBoolean);
