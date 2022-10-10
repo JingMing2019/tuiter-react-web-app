42752f9ed2daa440f322d7b2e9f984b2f06e09cf
jingming <mary.j.ming@gmail.com>
2022-10-09 22:56:45 -0700

    complete a5 javascript section 2.5.1 if else

--- public/labs/a5/index.js
+++ public/labs/a5/index.js
@@ -47,5 +47,17 @@ console.log(true3);
 console.log(true4);
 console.log(true5);
 console.log(false3);
-console.log(sortaTrue);
-console.log(notTrue);
+console.log('sortaTrue', sortaTrue);
+console.log('notTrue', notTrue);
+
+console.log('If else');
+if(true1) {
+    console.log(true);
+}
+
+if(!false1) {
+    console.log('!false1');
+} else {
+    console.log('false1');
+}
+
