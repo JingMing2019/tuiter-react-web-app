6e293833687af9a0051d50cc8cfb78163abe1c31
jingming <mary.j.ming@gmail.com>
2022-10-12 22:37:00 -0700

    complete a5 jquery section 2.11 binding to the DOM

--- public/labs/a5/index.js
+++ public/labs/a5/index.js
@@ -175,5 +175,10 @@ console.log(greeting2)
 const init = () => {
     console.log('Hello world from jQuery');
     /* do the rest of the lab work here */
+    const bindById = $('#bind-by-id');
+    const bindByClass = $('.bind-by-class');
+    console.log('Binding to DOM');
+    console.log(bindById);
+    console.log(bindByClass);
 }
 $(init);
