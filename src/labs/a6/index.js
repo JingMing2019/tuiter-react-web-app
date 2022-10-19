1443f7c9a7752e2b69de3e9936fbc2c743dca240
jingming <mary.j.ming@gmail.com>
2022-10-18 23:55:15 -0700

    complete a6 react section 2.10 work with HTML classes

--- src/labs/a6/index.js
+++ src/labs/a6/index.js
@@ -1,9 +1,11 @@
 // import {Link} from "react-router-dom"
+import Classes from "./classes";
 
 function Assignment6() {
     return(
         <div>
             <h1>Assignment 6</h1>
+            <Classes/>
         </div>
     );
 }
