d1efce05d6ca0938156c0a512bbef09febdb1907
jingming <mary.j.ming@gmail.com>
2022-10-19 14:53:31 -0700

    complete a6 react section 3.1.5 render dynamic classes with embedded expressions

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -11,15 +11,43 @@ const NavigationSidebar = (
         // The HTML class attribute is commonly used to associate CSS transformation rules to an HTML element.
         // In JavaScript class is a keyword so we can't use it, use className instead
         <div className="list-group">
-            <a className="list-group-item">Tuiter</a>
-            <a className="list-group-item">Home</a>
-            <a className="list-group-item">Explore</a>
-            <a className="list-group-item">Notifications</a>
-            <a className="list-group-item">Messages</a>
-            <a className="list-group-item">Bookmarks</a>
-            <a className="list-group-item">Lists</a>
-            <a className="list-group-item">Profile</a>
-            <a className="list-group-item">More</a>
+            <a className="list-group-item">
+                <span><i className="bi bi-twitter"/></span>
+                Tuiter
+            </a>
+            <a className={`list-group-item
+                    ${active === 'home'?'active':''}`}>
+                Home
+            </a>
+            <a className={`list-group-item
+                    ${active === 'explore'?'active':''}`}>
+                Explore
+            </a>
+            <a className={`list-group-item
+                    ${active === 'notifications'?'active':''}`}>
+                Notifications
+            </a>
+            <a className={`list-group-item
+                    ${active === 'messages'?'active':''}`}>
+                Messages
+            </a>
+            <a className={`list-group-item
+                    ${active === 'bookmarks'?'active':''}`}>
+                Bookmarks
+            </a>
+            <a className={`list-group-item
+                    ${active === 'lists'?'active':''}`}>
+                Lists
+            </a>
+            <a className={`list-group-item
+                    ${active === 'profile'?'active':''}`}>
+                Profile
+            </a>
+            <a className={`list-group-item
+                    ${active === 'more'?'active':''}`}>
+                More
+            </a>
+
         </div>
     );
 }
