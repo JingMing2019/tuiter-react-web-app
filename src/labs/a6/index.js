6ebb8d3e3d63e88901bcafa31941ec7dccfbaff2
jingming <mary.j.ming@gmail.com>
2022-10-18 23:43:23 -0700

    complete a6 react section 2.9 implement a navigation component

--- src/labs/a6/index.js
+++ src/labs/a6/index.js
@@ -1,11 +1,8 @@
-import {Link} from "react-router-dom"
+// import {Link} from "react-router-dom"
 
 function Assignment6() {
     return(
         <div>
-            <Link to="/">Lab</Link> |
-            <Link to="/hello">Hello</Link> |
-            <Link to="/tuiter">Tuiter</Link>
             <h1>Assignment 6</h1>
         </div>
     );
--- src/labs/index.js
+++ src/labs/index.js
@@ -1,8 +1,10 @@
+import Nav from "../nav";
 import Assignment6 from "./a6";
 
 function Labs() {
     return(
         <div>
+            <Nav/>
             {/*<h1>Assignment 6</h1>*/}
             <Assignment6/>
         </div>
