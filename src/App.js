675532291b5d444108ec6af9512ba2391f588f7b
jingming <mary.j.ming@gmail.com>
2022-10-18 23:31:06 -0700

    complete a6 react section 2.7 implement navigation in single page applications

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Labs from "./labs"
 import HelloWorld from "./labs/a6/hello-world";
 import Tuiter from "./tuiter";
+import {BrowserRouter} from "react-router-dom";
+import {Routes, Route} from "react-router";
 
 function App() {
   return (
@@ -10,12 +12,25 @@ function App() {
       // library called JSX or JavaScript XML. JSX allows mixing and matching JavaScript and XML seamlessly and HTML
       // is just a particular flavor of XML.
       // Note that in React.js we use className instead of class
-      <div className="container">
-        {/*<h1>Hello World!</h1>*/}
-          <HelloWorld/>
-          <Labs/>
-          <Tuiter/>
-      </div>
+      <BrowserRouter>
+          <div className="container">
+              {/*<h1>Hello World!</h1>*/}
+              <Routes>
+                  {/* Route component from React Router to declare paths and map them to corresponding component we want
+                  to render for that path*/}
+                  {/* index means this Labs component is the default landing screen */}
+                  <Route index element={<Labs/>}/>
+              </Routes>
+              <Routes>
+                  <Route path="/hello" element={<HelloWorld/>}/>
+              </Routes>
+              <Routes>
+                  <Route path="/tuiter" element={<Tuiter/>}/>
+              </Routes>
+          </div>
+      </BrowserRouter>
+
+
       // <div className="App">
     //   <header className="App-header">
     //     <img src={logo} className="App-logo" alt="logo" />
