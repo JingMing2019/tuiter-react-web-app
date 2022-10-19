274b3c8594086e375721bd0079504e73cc895b88
jingming <mary.j.ming@gmail.com>
2022-10-18 18:09:25 -0700

    complete a6 react section 2.1 implement single page applications

--- src/App.js
+++ src/App.js
@@ -3,22 +3,26 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      // This return statement is returning an HTML tag, not an HTML string. This is possible because React.js uses a
+      // library called JSX or JavaScript XML. JSX allows mixing and matching JavaScript and XML seamlessly and HTML
+      // is just a particular flavor of XML.
+      <h1>Hello World!</h1>
+      // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
