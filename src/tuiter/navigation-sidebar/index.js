5054b223d06e913f589edec2bb4e38bd2990493e
jingming <mary.j.ming@gmail.com>
2022-10-19 10:40:05 -0700

    complete a6 react section 3.1.4 use className

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -8,8 +8,18 @@ const NavigationSidebar = (
 ) => {
     // React.js function components can only return a single HTML element, can use <></> to replace
     return(
-        <div>
-
+        // The HTML class attribute is commonly used to associate CSS transformation rules to an HTML element.
+        // In JavaScript class is a keyword so we can't use it, use className instead
+        <div className="list-group">
+            <a className="list-group-item">Tuiter</a>
+            <a className="list-group-item">Home</a>
+            <a className="list-group-item">Explore</a>
+            <a className="list-group-item">Notifications</a>
+            <a className="list-group-item">Messages</a>
+            <a className="list-group-item">Bookmarks</a>
+            <a className="list-group-item">Lists</a>
+            <a className="list-group-item">Profile</a>
+            <a className="list-group-item">More</a>
         </div>
     );
 }
