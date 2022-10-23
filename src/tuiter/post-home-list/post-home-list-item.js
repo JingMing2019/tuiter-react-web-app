8660e14ede5259a2ec947e061e7980907fc02365
jingming <mary.j.ming@gmail.com>
2022-10-22 18:43:11 -0700

    refine a6 react section 4 home component

--- src/tuiter/post-home-list/post-home-list-item.js
+++ src/tuiter/post-home-list/post-home-list-item.js
@@ -1,57 +1,67 @@
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import InnerPost from "./inner-post";
+import posts from "./posts.json";
+import MarkTextAsLink from "./mark-text-as-link";
 
 const PostHomeListItem = (
     {
         post = {
-            userAvatar: "../../images/avatar-elonmusk.jpeg",
+            userAvatar: "avatar-elonmusk.jpeg",
             userName: "Elon Musk",
             userHandle: "elonmusk",
             time: "23h",
             title: "Amazing show about @Inspiration4x mission!",
             markText: "@Inspiration4x",
-            postImage: "../../images/post-elonmusk.jpeg",
-            postTitle: "Countdown: Inspiration4x Mission to Space | Netflix Office Site",
-            postContent: "From training to launch to landing, this all-access docuseries rides along with the Inspiration4 " +
-                "crew on the first all-civilian orbital space...",
-            postLinkName: "netflix.com",
-            postLink: "https://www.netflix.com/",
+            innerPost: {
+                type: "third-party",
+                tweetId: null,
+                postImage: "post-elonmusk.jpeg",
+                postTitle: "Countdown: Inspiration4x Mission to Space | Netflix Office Site",
+                postContent: "From training to launch to landing, this all-access docuseries rides along with the Inspiration4 " +
+                    "crew on the first all-civilian orbital space...",
+                postLinkName: "netflix.com",
+                postLink: "https://www.netflix.com/",
+            },
             reply: "4.2K",
             retweet: "3.5K",
             like: "37.5K",
+            retweeted: ""
         }
     }
 ) => {
-    const roundedTop = {
-        overflow: "hidden",
-        "borderRadius": "1rem 1rem 0 0"
+    const filterById = (jsonObject, id) => {
+        return jsonObject.filter(function(jsonObject) {return (jsonObject['_id'] === id);})[0];
     }
 
-    const markKeywordAsLink = (item, keyword) => {
-        let re = new RegExp(keyword, 'g')
-        return (
-            item.replace(re, '<a href="/#" class="text-decoration-none text-primary">'+ keyword +'</a>')
-        )
+    const fontSize = {
+        "fontSize": "14px"
     }
 
-    // Hiding post body when there is no post title and post content, and edit post image rounded corner accordingly
-    let postImage = <img src={post.postImage} className="w-100 rounded-4" alt="Post"/>;
-    let postBody = false;
-    if (post.postTitle || post.postContent) {
-        postImage = <img src={post.postImage} className="w-100" style={roundedTop} alt="Post"/>;
-        postBody = true;
-    }
     return(
         <div className="list-group-item pt-3 wd-post-list-item border border-light">
+            {post.retweeted &&
+                <div className="row align-items-center mb-1">
+                    {/* Left icon */}
+                    <div className="col-1 px-0">
+                        <FontAwesomeIcon icon="fa-solid fa-retweet" className="float-end" />
+                    </div>
+                    {/* Right Person */}
+                    <div className="col-11 ps-4 text-secondary fw-bold">
+                        <span style={fontSize}>{post.retweeted} {" "} Retweeted</span>
+                    </div>
+                </div>
+            }
             <div className="row">
                 {/* Left Avatar */}
                 <div className="col-1">
-                    <img src={post.userAvatar} className="rounded-circle" width="48px" alt="avatar"/>
+                    <img src={`/images/${post.userAvatar}`} className="rounded-circle" width="48px" alt="avatar"/>
                 </div>
                 {/* Right Content */}
                 <div className="col-11 ps-4">
+                    {/* User's name handle and post time */}
                     <div className="row align-items-center">
                         <div className="col-11">
-                            {post.userName}{' '}
+                            <span className="fw-bolder">{post.userName}{' '}</span>
                             <FontAwesomeIcon icon="fa-solid fa-circle-check" className="text-primary"/>
                             <span className="text-secondary">{' '}@{post.userHandle} {'\u00B7'} {post.time}</span>
                         </div>
@@ -60,22 +70,11 @@ const PostHomeListItem = (
                         </div>
                     </div>
                     {/* Post Title */}
-                    {/* like innerHTML in the browser*/}
-                    <div dangerouslySetInnerHTML={{__html: markKeywordAsLink(post.title, post.markText)}}/>
+                    {post.markText && <MarkTextAsLink text={post.title} markText={post.markText}/>}
+                    {!post.markText && post.title}
                     {/* Inner Post */}
-                    <div className="border rounded-4 my-3">
-                        {postImage}
-                        {postBody &&
-                            <div className="border-top px-3 py-3">
-                                <h6 className="mb-0">{post.postTitle}</h6>
-                                <p className="text-secondary mb-0">{post.postContent}</p>
-                                <a href={post.postLink} className="text-secondary text-decoration-none">
-                                    <FontAwesomeIcon icon="fa-solid fa-link"/>
-                                    {post.postLinkName}
-                                </a>
-                            </div>
-                        }
-                    </div>
+                    <InnerPost post={post.innerPost}
+                               tweet={post.innerPost.type === "tweet" ? filterById(posts, post.innerPost.tweetId): null}/>
                     {/* Bottom Icons */}
                     <div className="row mb-2">
                         <a href="/#" className="col text-secondary text-decoration-none">
@@ -94,6 +93,12 @@ const PostHomeListItem = (
                             <FontAwesomeIcon icon="fa-solid fa-arrow-up-from-bracket" className="pe-2" />
                         </a>
                     </div>
+                    {/*  If retweeted, show this thread  */}
+                    {post.retweeted &&
+                        <div className="my-3">
+                            <a href="/#" className="text-decoration-none text-primary">Show this thread</a>
+                        </div>
+                    }
                 </div>
             </div>
         </div>
