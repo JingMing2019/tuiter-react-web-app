811324bd2b846dc44764395306baa7003740a74f
jingming <mary.j.ming@gmail.com>
2022-10-26 22:19:11 -0700

    complete a7 redux section 2.4 create hello world redux component

--- src/labs/a7/redux-examples/index.js
+++ src/labs/a7/redux-examples/index.js
@@ -1,10 +1,21 @@
 import React from "react";
+import HelloReduxExampleComponent from "./hello-redux-example-component";
+import hello from "./reducers/hello";
+import {Provider} from "react-redux";
+import {configureStore} from "@reduxjs/toolkit";
+
+const store = configureStore(hello);
 
 const ReduxExamples = () => {
     return(
-        <div>
-            <h2>Redux Examples</h2>
-        </div>
+        // Provider delivers data in store to child elements
+        <Provider store={store}>
+            <div>
+                <h2>Redux Examples</h2>
+                {/* component consumes the data */}
+                <HelloReduxExampleComponent />
+            </div>
+        </Provider>
     )
 }
 
