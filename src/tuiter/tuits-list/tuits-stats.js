b5792342843403e40e066f7a11a88edb8aec4436
jingming <mary.j.ming@gmail.com>
2022-11-17 20:12:33 -0800

    complete a8 node section 6.5 implement dislike counter

--- src/tuiter/tuits-list/tuits-stats.js
+++ src/tuiter/tuits-list/tuits-stats.js
@@ -1,5 +1,5 @@
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { faHeart, faThumbsDown } from "@fortawesome/free-solid-svg-icons";
 import {updateTuitThunk} from "../../services/tuits-thunks";
 import {useDispatch} from "react-redux";
 
@@ -13,9 +13,11 @@ const TuitsStats = (
             title: "Autopilot and Full Self-Driving Capability",
             image: "tesla-logo.png",
             liked: false,
+            disliked: false,
             replies: 234,
             retuits: 543,
             likes: 3456,
+            dislikes: 123,
             handle: "@tesla",
             tuit: "Autopilot is an advanced driver assistance system that enhances safety and convenience behind the wheel. When used properly, Autopilot reduces your overall workload as a driver. Each new Tesla vehicle is equipped with eight external cameras and powerful vision processing to provide an additional layer of safety. All vehicles built for the North American market now use our camera-based Tesla Vision to deliver Autopilot features, rather than radar. All vehicles built for the North American market now use our camera-based Tesla Vision to deliver Autopilot features, rather than radar."
         }
@@ -23,11 +25,20 @@ const TuitsStats = (
 ) => {
     const dispatch = useDispatch();
 
-    const clickHeartHandler = () => {
+    const clickLikeHeartHandler = () => {
         const updatedTuit = {
             ...tuit,
-            likes : tuit.likes + 1,
-            liked : true
+            likes : tuit.liked ? tuit.likes - 1 : tuit.likes + 1,
+            liked : !tuit.liked
+        };
+        dispatch(updateTuitThunk(updatedTuit))
+    }
+
+    const clickDislikeThumbHandler = () => {
+        const updatedTuit = {
+            ...tuit,
+            dislikes : tuit.disliked ? tuit.dislikes - 1 : tuit.dislikes + 1,
+            disliked : !tuit.disliked
         };
         dispatch(updateTuitThunk(updatedTuit))
     }
@@ -43,10 +54,15 @@ const TuitsStats = (
                     <FontAwesomeIcon icon="fa-solid fa-retweet" className="pe-1 pe-sm-2" />
                     <span>{tuit.retuits}</span>
                 </button>
-                <button className="col text-secondary border-0 bg-transparent text-start">
+                <button className="col text-secondary border-0 bg-transparent text-start" onClick={clickLikeHeartHandler}>
                     {tuit.liked && <FontAwesomeIcon icon={faHeart} color={ 'red' } className="pe-1 pe-sm-2"/>}
-                    {!tuit.liked && <FontAwesomeIcon icon="fa-regular fa-heart" className="pe-1 pe-sm-2" onClick={clickHeartHandler}/>}
+                    {!tuit.liked && <FontAwesomeIcon icon="fa-regular fa-heart" className="pe-1 pe-sm-2" />}
                     <span>{tuit.likes}</span>
+                </button>
+                <button className="col text-secondary border-0 bg-transparent text-start" onClick={clickDislikeThumbHandler}>
+                    {tuit.disliked && <FontAwesomeIcon icon={faThumbsDown} color={ 'gray' } className="pe-1 pe-sm-2"/>}
+                    {!tuit.disliked && <FontAwesomeIcon icon="fa-regular fa-thumbs-down" className="pe-1 pe-sm-2"/>}
+                    <span>{tuit.dislikes}</span>
                 </button>
                 <button className="col text-secondary border-0 bg-transparent text-start">
                     <FontAwesomeIcon icon="fa-solid fa-arrow-up-from-bracket" className="pe-1 pe-sm-2" />
